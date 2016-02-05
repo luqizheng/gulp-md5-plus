a80ccb06875ec81ec51547b5f3745b16f4756870
luqizheng <lqz.leo@gmail.com>
2016-02-05 14:19:53 +0800

    能够多次replace已经被md5过的路径

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var path = require('path')
 , glob = require('glob');
 
 module.exports = function (size, ifile) {
+
     size = size | 0;
     return through.obj(function (file, enc, cb) {
         if (file.isStream()) {
@@ -13,47 +14,62 @@ module.exports = function (size, ifile) {
             return cb();
         }
 
-        if(!file.contents){
+        if (!file.contents) {
             return cb();
         }
 
         var d = calcMd5(file, size)
         , filename = path.basename(file.path)
-        , relativepath = path.relative(file.base ,file.path)
-        , sub_namepath = relativepath.replace(new RegExp(filename) , "").split(path.sep).join('/')
+        , relativepath = path.relative(file.base, file.path)
+        , sub_namepath = relativepath.replace(new RegExp(filename), "").split(path.sep).join('/')
         , dir;
 
-        if(file.path[0] == '.'){
+        if (file.path[0] == '.') {
             dir = path.join(file.base, file.path);
         } else {
             dir = file.path;
         }
         dir = path.dirname(dir);
 
-        var md5_filename = filename.split('.').map(function(item, i, arr){
-            return i == arr.length-2 ? item + '_'+ d : item;
+        var md5_filename = filename.split('.').map(function (item, i, arr) {
+            return i == arr.length - 2 ? item + '_' + d : item;
         }).join('.');
 
-        if(Object.prototype.toString.call(ifile) == "[object Array]"){
-            ifile.forEach(function(i_ifile){
-                i_ifile && glob(i_ifile,function(err, i_files){
-                    if(err) return console.log(err);
-                    i_files.forEach(function(i_ilist){
-                        var result = fs.readFileSync(i_ilist,'utf8').replace(new RegExp('/' + filename + '[^a-zA-Z_0-9].*?' ,"g"), function(sfile_name){
-                        return sfile_name.replace(filename,md5_filename)
-                    });
+        console.log(md5_filename);
+
+        if (Object.prototype.toString.call(ifile) == "[object Array]") {
+            ifile.forEach(function (i_ifile) {
+                i_ifile && glob(i_ifile, function (err, i_files) {
+                    if (err) return console.log(err);
+                    i_files.forEach(function (i_ilist) {
+                        var ary = filename.split('.');
+                        var ext = ary.pop();
+                        var md5FileName = ary.join('.');
+                        var regexd = '(/' + filename + '[^a-zA-Z_0-9].*?)|' + '(/' + md5FileName + '_[a-zA-Z_0-9]+.' + ext + '?)'                        
+                        var result = fs.readFileSync(i_ilist, 'utf8').replace(new RegExp(regexd, "g"), function (sfile_name) {                            
+                            return sfile_name.replace(sfile_name, '/'+md5_filename)
+                        });
                         fs.writeFileSync(i_ilist, result, 'utf8');
                     })
                 })
             })
-        }else{
-            ifile && glob(ifile,function(err, files){
-                if(err) return console.log(err);
-                files.forEach(function(ilist){
-                    var result = fs.readFileSync(ilist,'utf8').replace(new RegExp('/' + filename + '[^a-zA-Z_0-9].*?' ,"g"), function(sfile_name){
-                        return sfile_name.replace(filename,md5_filename)
+        } else {
+            ifile && glob(ifile, function (err, files) {
+                if (err) return console.log(err);
+                files.forEach(function (ilist) {
+                    /*var result = fs.readFileSync(ilist, 'utf8').replace(new RegExp('/' + filename + '[^a-zA-Z_0-9].*?', "g"), function (sfile_name) {                        
+                        return sfile_name.replace(filename, md5_filename)
                     });
-                    fs.writeFileSync(ilist, result, 'utf8');
+                    fs.writeFileSync(ilist, result, 'utf8');*/
+
+                    var ary = filename.split('.');
+                    var ext = ary.pop();
+                    var md5FileName = ary.join('.');
+                    var regexd = '(/' + filename + '[^a-zA-Z_0-9].*?)|' + '(/' + md5FileName + '_[a-zA-Z_0-9]+.' + ext + '?)'
+                    var result = fs.readFileSync(i_ilist, 'utf8').replace(new RegExp(regexd, "g"), function (sfile_name) {
+                        return sfile_name.replace("/" + sfile_name, '/'+md5_filename)
+                    });
+                    fs.writeFileSync(i_ilist, result, 'utf8');
                 })
             })
         }
@@ -68,9 +84,9 @@ module.exports = function (size, ifile) {
 };
 
 
-function calcMd5(file, slice){
+function calcMd5(file, slice) {
     var md5 = crypto.createHash('md5');
     md5.update(file.contents, 'utf8');
 
-    return slice >0 ? md5.digest('hex').slice(0, slice) : md5.digest('hex');
+    return slice > 0 ? md5.digest('hex').slice(0, slice) : md5.digest('hex');
 }
